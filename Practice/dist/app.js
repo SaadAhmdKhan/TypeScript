1effe497e62d0cb013e8d951d7452eee537430ac
Saad Ahmed Khan <saadahmedkhan578@gmail.com>
2025-02-16 16:23:11 +0500

    Add Practice

---
+++ Practice/dist/app.js
@@ -0,0 +1,26 @@
+"use strict";
+// let inp1 = document.getElementById("inp1") ! as HTMLInputElement
+// let inp2 = document.getElementById("inp2") ! as HTMLInputElement
+// let addBtn = document.getElementById("addBtn") !
+// function sumOffTwo(firtsNumber :number , secondNumber : number){
+//     return firtsNumber + secondNumber
+// }
+// addBtn.addEventListener("click" , function(){
+//     console.log(sumOffTwo(+inp1.value , +inp2.value))
+// })
+// var userName : number | string
+// userName = "ali"
+// userName = 50
+// console.log(userName)
+// let cities: string[] = []
+// cities.push("20")
+// console.log(cities)
+// var userName: any
+// userName = 'ali'
+// userName = 70
+// userName = false
+var randomData; // tuples fix length array with it's types\
+randomData = ['john', 0];
+randomData[] = 0;
+console.log(randomData);
+//# sourceMappingURL=app.js.map
